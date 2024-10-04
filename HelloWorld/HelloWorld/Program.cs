c86d673cdef12a797dc6b02b48d16cc4c1a09275
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-04 12:56:28 -0300

    .

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,10 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello, World!");
-
-            //Console.ReadKey();
+            
         }
     }
 }
