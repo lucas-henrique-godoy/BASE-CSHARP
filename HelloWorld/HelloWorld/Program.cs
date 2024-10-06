b64188ddf4edfaa9b69f224d962a6c46ee6e997f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-06 00:41:09 -0300

    adicionei explicações sobre tipos e  nomes de variáveis

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,6 +10,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // TIPOS
+            // Int: - 156 como 32334
+            // Float: - 15.6 como 232.132
+            // Bool: true ou false
+            // String: "dasda dfsaasdas"
+            // Char: 'a'
+
+            // REGRA DE NOMES PARA VARIÁVEIS
+            // A-Z devem começar com letras
+            // 0-9 não pode começar com um  número
+            // @ $ . não pode conter caratecteres especiais
+            // _ pode ter underline
+            // não pode usar palavras reservadas
+            int segundaGuerraMundial
+            
             Console.WriteLine("Hello, Wolrd!");
 
             Console.WriteLine("Meu nome é Lucas!");
