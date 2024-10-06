f86d75cedf9e8a8cc198a1bcbe540fc9d3cc609f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-06 17:43:29 -0300

    coloquei exemplos de de declarações de variáveis.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -24,12 +24,43 @@ namespace HelloWorld
             // _ pode ter underline
             // não pode usar palavras reservadas
 
+            /* Forma mais usada de declarar variáveis.
             int segundaGuerraMundial = 1939;
             string corFavorita = "Preto";
+            float velocidadeF1 = 294.98f;
+            bool segundaGuerraMundialAconteceu = true;
             
-            Console.WriteLine("Hello, Wolrd!");
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(segundaGuerraMundialAconteceu);
 
-            Console.WriteLine("Meu nome é Lucas!");
+            velocidadeF1 = 348.29f;
+            corFavorita = "Vermelho";
+
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(corFavorita);
+            */
+
+            /* Uso do  var para declarar variáveis sem passar o tipo.
+            var corFavorita = "vermelho";
+            var modeloDoProduto = 2323; 
+            */
+
+            /*Usando tipagem dinamica(é possivel alterar o tipo da variável mesmo depis de declarada)
+            NÃO RECOMENDADO O USO.
+            dynamic corFavorita = "Azul";
+            Console.WriteLine(corFavorita);
+
+            corFavorita = 2121;
+            Console.WriteLine(corFavorita);
+
+            corFavorita = 232.55;
+            Console.WriteLine(corFavorita);
+
+            corFavorita = false;
+            Console.WriteLine(corFavorita);
+            */
 
             Console.ReadLine();
         }
