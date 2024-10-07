2940542b819e1a58d9a4de1fd2fd986e3d4d4de8
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-07 11:13:02 -0300

    .

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -74,18 +74,18 @@ namespace HelloWorld
             Console.WriteLine("Bem Vindo! " + nome);
             */
 
-            //OPERADORES ARITMÉTICOS
+            /* OPERADORES ARITMÉTICOS
             int numeroQualquer = 20 + 20 - 80;
             int numeroMult = 10 * 90;
             float numeroDiv = 5.0f / 2;
 
-            int teste = 2 + 2 * 10;
-
+            int teste = (2 + 2) * 10;
 
             Console.WriteLine(numeroQualquer);
             Console.WriteLine(numeroMult);
             Console.WriteLine(numeroDiv);
             Console.WriteLine(teste);
+            */
 
 
 
