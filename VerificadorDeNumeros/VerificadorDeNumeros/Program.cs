668b25091227262c5d879dcaa8ab1c5cd5cf69ec
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-08 07:40:43 -0300

    criei um exercício de verificador de números com if else  else if com C#.

---
+++ VerificadorDeNumeros/VerificadorDeNumeros/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificadorDeNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número entre 1 e 100");
+            int numero = int.Parse(Console.ReadLine());
+
+            if( numero % 2 == 0)
+            {
+                Console.WriteLine("O número é par!");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar!");
+            }
+
+            if(numero >= 1 && numero <= 100)
+            {
+                Console.WriteLine("O número esta dentro do intervalo.");
+            }
+            else
+            {
+                Console.WriteLine("O número está fora do intervalo.");
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VerificadorDeNumeros/VerificadorDeNumeros/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VerificadorDeNumeros")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VerificadorDeNumeros")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fdc50970-7bff-4c3f-a6f7-9eb1196099db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
