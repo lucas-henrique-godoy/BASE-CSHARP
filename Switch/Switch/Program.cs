f6421e1dc49d50116c643a808a08663389141cb3
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-08 16:35:53 -0300

    slavei o arquivo com o switch

--- Switch/Switch/Program.cs
+++ Switch/Switch/Program.cs
@@ -23,12 +23,18 @@ namespace Switch
                 case "Azul":
                     Console.WriteLine("Sua cor favotita é Azul!");
                     break;
+                case "Rosa":
+                    Console.WriteLine("Sua cor favotita é Rosa!");
+                    break;
                 default:
                     Console.WriteLine("Você não escolheu nenhuma cor disponível!");
                     break;
             }
-            
-            
+
+            /*
+             O switch em C# é projetado para funcionar apenas com comparações diretas, ou seja, ele avalia uma expressão e compara seu resultado com valores específicos definidos em seus casos (case).
+             Isso significa que você não pode usar operadores condicionais como > (maior que), < (menor que), ou != (diferente) dentro de um switch.
+             */
             Console.ReadLine();
         }
     }
