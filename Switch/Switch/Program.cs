a2fe84117f7b47defe8caffd4cf6bcb6efbac60f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-08 16:30:31 -0300

    criei u projeto para explicar o switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cor = "Azul";
+
+            switch (cor)
+            {
+                case "Vermelho":
+                    Console.WriteLine("Sua cor favotita é Vermelho!");
+                    break;
+                case "Amarelo":
+                    Console.WriteLine("Sua cor favotita é Amarelo!");
+                    break;
+                case "Azul":
+                    Console.WriteLine("Sua cor favotita é Azul!");
+                    break;
+                default:
+                    Console.WriteLine("Você não escolheu nenhuma cor disponível!");
+                    break;
+            }
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
