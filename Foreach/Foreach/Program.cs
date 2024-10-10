d51af73abf51e5c6b3f475a0e40e4af139c8b385
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-10 20:14:44 -0300

    .

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Foreach usado para percorrer arrays.
+            
+            string[] palavras = { "Victor", "Lima", "Guia do prog", "Formação", "Node", "Curso de C#", "Udemy"};           
+            
+            foreach (string palavra in palavras) // Para cada.
+            {
+                Console.WriteLine(palavra);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
