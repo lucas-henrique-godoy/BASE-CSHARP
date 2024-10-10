746e00f0b211997bebefec95032845e6c2661d71
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-10 19:51:38 -0300

    criei um novo projeto explicando o Do While em C#

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                
+            }
+            while (true);
+
+
+            Console.ReadLine();
+        }
+    }
+}
