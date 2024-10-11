dd41396399522f3576b0ad6b6f3b43a98022fa55
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-10 21:46:18 -0300

    Criei o projeto  explicando Escopo e C#

---
+++ Escopo/Escopo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Escopo
+{
+    internal class Program
+    {
+        // Variável de escopo global (estática).
+        static string meuNomeCompleto = "Lucas Henrique Godoy";
+
+        // Para acessar variáveis globais em classes estáticas, a variável também deve ser static.
+        // No C#, cada variável só pode ser acessada dentro do escopo onde foi criada.
+
+        static void Main(string[] args)
+        {
+            // Variável de escopo local.
+            string meuNome = "Lucas";
+
+            // Imprime a variável de escopo global.
+            Console.WriteLine(meuNomeCompleto);
+
+            // Imprime a variável de escopo local.
+            Console.WriteLine(meuNome);
+
+            Console.ReadLine();
+        }
+
+        // Método que tenta acessar a variável global.
+        public void mostrarMensagem()
+        {
+            Console.WriteLine(meuNomeCompleto); // Acessa a variável de escopo global.
+            // Não pode acessar 'meuNome' aqui, pois está fora de escopo.
+        }
+    }
+}
